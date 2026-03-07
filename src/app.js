7f99b72069c90ec648076aa1b1976a4aeb17116e
Ewerton Hecsley <ewerton.martinscomercial@gmail.com>
2026-03-07 19:30:31 -0300

    config: finalized configuration of server

---
+++ src/app.js
@@ -0,0 +1,30 @@
+// app.js
+require('dotenv').config();
+const { app } = require('./index');
+const { validateEnviroment, Logger } = require('./config/validateEnviroment');
+const { gracefulShutdown } = require('./utils/gracefullShutdown');
+
+const env = validateEnviroment();
+
+async function startServer() {
+    try {
+        Logger.info(`Iniciando aplicação em modo ${env.NODE_ENV}...`);
+
+        const server = app.listen(env.PORT, () => {
+            Logger.info(`Servidor rodando na porta ${env.PORT}`);
+        });
+
+        const shutdownHandler = gracefulShutdown(server);
+
+        process.on('SIGINT', ()=> shutdownHandler('SIGINT'));
+        process.on('SIGTERM', ()=> shutdownHandler('SIGTERM'));
+        
+    } catch (error) {
+        Logger.error(`Erro ao iniciar o servidor: ${error.message}`);
+        process.exit(1);
+    }
+}
+
+module.exports = {
+    startServer
+}
---
+++ src/config/validateEnviroment.js
@@ -0,0 +1,39 @@
+const Logger = {
+  info: (msg) => console.log(`\x1b[36m[INFO]\x1b[0m [${new Date().toISOString()}] ${msg}`),
+  error: (msg) => console.error(`\x1b[31m[ERROR]\x1b[0m [${new Date().toISOString()}] ${msg}`),
+  warn: (msg) => console.warn(`\x1b[33m[WARN]\x1b[0m [${new Date().toISOString()}] ${msg}`)
+};
+
+const Environment = Object.freeze({
+  PRODUCTION: 'production',
+  DEVELOPMENT: 'development'
+});
+
+const validateEnviroment = () =>{
+    Logger.info('Validando variáveis de ambiente...');
+
+    const requiredEnvVars = ['NODE_ENV', 'PORT'];
+    const missingEnvVars = requiredEnvVars.filter(envVar => !process.env[envVar]);
+
+    if (missingEnvVars.length > 0) {
+        Logger.error(`Falha critica: Variáveis ausentes -> ${missingEnvVars.join(', ')}`);
+        process.exit(1);
+    }
+
+    if (!Object.values(Environment).includes(process.env.NODE_ENV)) {
+        Logger.error(`Falha critica: Valor inválido para NODE_ENV: ${process.env.NODE_ENV}. Deve ser 'production' ou 'development'.`);
+        process.exit(1);
+    }
+
+    Logger.info("✅ Ambiente validado com sucesso!");
+
+    return {
+        NODE_ENV: process.env.NODE_ENV,
+        PORT: process.env.PORT
+    };
+}
+
+module.exports = {
+    validateEnviroment,
+    Logger
+}
